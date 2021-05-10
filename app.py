ca6f634ee494a4682eb81f283eddb2fcd161b7da
RicksonMonteiroENCAUT <ricksonencaut@gmail.com>
2021-05-09 22:23:08 -0300

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from joblib import load
 import numpy as np
+import os
 app = Flask(__name__)
 
 #Load model
@@ -18,4 +19,5 @@ def post():
     return jsonify({"Valor":round(float(value),2)})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
